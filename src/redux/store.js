0813933f49d9c2ff79bf5cd73a192735af26b574
Meet Makwana <meetmakwana203@gmail.com>
2023-10-10 16:39:33 +0530

    Added redux rtk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducer";
 
-const dummyReducer = () => {
-    return "Dummy reducer";
-}
+// const dummyReducer = () => {
+//   return "Dummy reducer";
+// };
 
 // const store = createStore(dummyReducer)
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
+const store = configureStore({ reducer: rootReducer });
 
 export default store;

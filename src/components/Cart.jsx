3e5f34fba42a88d3cdef19c6b8438fb5efee6d13
Meet Makwana <meetmakwana203@gmail.com>
2023-10-11 10:31:47 +0530

    Cart page done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,66 @@
-export default function Cart(){
-    return(
-        <div>
-            <h1>Cart</h1>
-        </div>
-    )
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Cart() {
+  const cartData = useSelector((state) => state.cartData);
+  console.log("Cart data is ", cartData);
+
+  let sum =
+    cartData.length &&
+    cartData.map((item) => item.price).reduce((prev, next) => prev + next);
+
+  return (
+    <div className="d-flex p-5 justify-content-center">
+      <div className="cart-container d-flex">
+        <table>
+          <tr>
+            <td></td>
+            <td>
+              <b>Name</b>
+            </td>
+            <td>
+              <b>Price</b>
+            </td>
+          </tr>
+          {cartData &&
+            cartData.map((item) => {
+              return (
+                <tr key={item.id} className="">
+                  <td>
+                    <img src={item.image} alt="Product" width={70} />
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                </tr>
+              );
+            })}
+        </table>
+      </div>
+      <table className="price-details">
+        <tr>
+          <td>
+            <b>Sum</b>
+          </td>
+          <td>{sum}</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Tax</b>
+          </td>
+          <td>12%</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Discount</b>
+          </td>
+          <td>25%</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Total</b>
+          </td>
+          <td>{sum - (sum*0.012) - (sum*0.25)}</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
